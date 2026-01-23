25f1197be92169bd5850dd914e3c850b9d3cfef3
DecodeSans <bsanskritee23@gmail.com>
2026-01-23 22:37:18 +0530

    navbar changes

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,12 @@
 import "./Navbar.css";
+import { FaBars } from "react-icons/fa";
 
 export default function Navbar({ toggleSidebar }) {
   return (
     <header className="navbar">
-      
+      <button className="menu-btn" onClick={toggleSidebar}>
+        <FaBars />
+      </button>
       
       <button className="offer">Free offer</button>
     </header>
